6754bd8d0fa43ed90eda67f27906cc95f159d631
surajwebo <surajmirajkar@rediffmail.com>
2014-02-12 15:57:34 +0530

    UILabel as a Placeholder in UItextView.
    Code completed.

--- PlaceholderInUITextView/ViewController.m
+++ PlaceholderInUITextView/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *lblPlaceHolderText;
+@property (weak, nonatomic) IBOutlet UITextView *textViewForMessage;
+
 @end
 
 @implementation ViewController
@@ -24,6 +27,18 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma UITextView Delegate
+- (void)textViewDidChange:(UITextView *)textView {
+    // Enable and disable lblPlaceHolderText
+    if ([textView.text length] > 0) {
+        [textView setBackgroundColor:[UIColor whiteColor]];
+        [self.lblPlaceHolderText setHidden:YES];
+    } else {
+        [textView setBackgroundColor:[UIColor clearColor]];
+        [self.lblPlaceHolderText setHidden:NO];
+    }
 }
 
 @end
